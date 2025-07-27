07120ac6ce19f293942ff533c1d7d29c10f1f410
Humphrey Mubweka <mubwekahumphrey@gmail.com>
2025-07-27 14:07:31 +0300

    Add updateRecipe and deleteRecipe actions to recipeStore

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,13 +1,24 @@
-
 import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
-  setRecipes: (recipes) => set({ recipes }),
+
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
 
 export default useRecipeStore;
